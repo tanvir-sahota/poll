ad62f2863adfd486bf89dfbc87b1636e7a4e5c15
tanvir-sahota <tanvirsahota07@gmail.com>
2024-01-31 21:56:01 +0000

    Create frontend & backend template
    The react app is created for the front end. Very basic express and mongoose server code has also been added.

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Successfuly connected to database.')
+
+    app.use(express.json())
+
+    app.get('/', (req, res) => {
+      res.send('Hello World!')
+    })
+    
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
+
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
