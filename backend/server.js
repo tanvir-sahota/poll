6889f732733e10c0743ebebd3f6c6cbf79b7912a
Omar Yahya <omar.yahya@kcl.ac.uk>
2024-02-05 10:22:40 +0000

    Add nodemon devdependency

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI)
     app.get('/', (req, res) => {
       res.send('Hello World!')
     })
-    
+
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`)
     })
