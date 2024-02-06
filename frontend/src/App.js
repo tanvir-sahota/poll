ce54e55a0889b42335459b5689038d2d434fdfd0
Armaan Uddin <armaan.uddin@kcl.ac.uk>
2024-02-06 23:30:05 +0000

    Create dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -18,6 +18,28 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );
+}*/
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Dashboard />} 
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
