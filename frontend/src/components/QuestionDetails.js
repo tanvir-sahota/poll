e2048720426235e066ac8921f3e5b72c429f537f
SebKCL <k22008004@kcl.ac.uk>
2024-02-07 14:26:55 +0000

    "Create Question Details and useQuestionContext"

---
+++ frontend/src/components/QuestionDetails.js
@@ -0,0 +1,31 @@
+import { useQuestionContext } from "../hooks/useQuestionContext"
+
+const QuestionDetails = ({ question }) => {
+    
+    const { dispatch } = useQuestionContext()
+
+    const handleClick = async () => {
+        const response = await fetch("/api/workouts/" + workout._id, {
+            method: "DELETE"
+        })
+        const json = await response.json()
+        
+        if(response.ok){
+            dispatch({type: "DELETE_QUESTIONS", payload: json})
+        }
+
+    }
+    
+    return (
+        <div className="question-details">
+            <h4>{question.question}</h4>
+            <p><strong>Options: </strong>{question.options}</p>
+            <p><strong>Answer(s): </strong>{question.answers}</p>
+            <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>delete</span>
+
+        </div>
+    )
+} 
+
+export default QuestionDetails
