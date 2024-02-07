9a3360334ca1232b552c7d2d5a1a8faf0e48317c
tanvir-sahota <tanvirsahota07@gmail.com>
2024-02-07 16:29:56 +0000

    Change server port This is so that the react development server can run on port 3000, while the express API server runs on port 4000.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 const userRoutes = require('./routes/users')
 
 const mongoose = require('mongoose')
