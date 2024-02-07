ff25115c79fa43e537efb3d806c77ad9e1f23974
tanvir-sahota <tanvirsahota07@gmail.com>
2024-02-07 22:16:09 +0000

    Use bootstrap for sign up form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,3 @@
-import './App.css';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignupForm from './components/SignupForm'
 
--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -1,58 +1,74 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Form, Button } from "react-bootstrap";
 
 const SignupForm = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState(null)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const user = {username, password}
-    
-    const response = await fetch('http://localhost:4000/api/users', {
-      method: 'POST',
+    const user = { username, password };
+
+    const response = await fetch("http://localhost:4000/api/users", {
+      method: "POST",
       body: JSON.stringify(user),
       headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    const json = await response.json()
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
 
     if (!response.ok) {
-      setError(json.error)
-      console.log('new user NOT added:', json)
+      setError(json.error);
+      console.log("new user NOT added:", json);
     }
     if (response.ok) {
-      setError(null)
-      setUsername('')
-      setPassword('')
-      console.log('new user added:', json)
+      setError(null);
+      setUsername("");
+      setPassword("");
+      console.log("new user added:", json);
     }
-  }
+  };
 
   return (
-    <form className="sign-up" onSubmit={handleSubmit}> 
-      <h3>Sign Up</h3>
+    <div
+      className="form-container"
+      style={{ display: "block", width: 700, padding: 30 }}
+    >
+      <Form className="sign-up" onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+        <Form.Group className="mb-3">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            placeholder="Enter username"
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+        </Form.Group>
 
-      <label>Username:</label>
-      <input 
-        type="text" 
-        onChange={(e) => setUsername(e.target.value)} 
-        value={username}
-      />
+        <Form.Group className="mb-3">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            placeholder="Password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </Form.Group>
 
-      <label>Password:</label>
-      <input 
-        type="password" 
-        onChange={(e) => setPassword(e.target.value)} 
-        value={password}
-      />
+        <Button variant="primary" type="submit">
+          Sign Up
+        </Button>
+        {error && <div className="error">{error}</div>}
+      </Form>
+    </div>
+  );
+};
 
-      <button>Sign Up</button>
-      {error && <div className="error">{error}</div>}
-    </form>
-  )
-}
-
-export default SignupForm
+export default SignupForm;
