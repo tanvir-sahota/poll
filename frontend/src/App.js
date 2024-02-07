5cdf2a8541f39988eeb03fb826a23e881a027727
tanvir-sahota <tanvirsahota07@gmail.com>
2024-02-07 22:57:27 +0000

    Route sign up form to sign_up route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route 
-              path="/" 
+              path="/sign_up" 
               element={<SignupForm />} 
             />
           </Routes>
