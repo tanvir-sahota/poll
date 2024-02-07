4b8be853ac463eabb465081835815a6d8f59985c
Saihan786 <marshall.s15@hotmail.com>
2024-02-07 20:59:13 +0000

    Refactor "quizzes" route to use controller functions

--- backend/routes/quizzes.js
+++ backend/routes/quizzes.js
@@ -1,33 +1,30 @@
 const express = require('express')
+const {
+    get_all_quizzes,
+    get_one_quiz,
+    create_quiz,
+    delete_quiz,
+    patch_quiz,
+} = require('../controllers/quiz_controller')
+const Quiz = require("../models/quiz_model")
 
 const router = express.Router()
 
 // Paths are relative to /api/quizzes, e.g. the below is /api/quizzes/
 
 // Retrieves all of the quizzes
-router.get('/', (request, response) => {
-    response.json({mssg: 'GET all quizzes'})
-})
+router.get('/', get_all_quizzes)
 
 // Retrieves a single quiz
-router.get('/:id', (request, response) => {
-    response.json({mssg: 'GET a single quiz'})
-})
+router.get('/:id', get_one_quiz)
 
 // Post a new quiz
-router.post('/', (request, response) => {
-    //request.body
-    response.json({mssg: 'POST a new quiz'})
-})
+router.post('/', create_quiz)
 
 // Delete a quiz
-router.delete('/:id', (request, response) => {
-    response.json({mssg: 'DELETE a quiz'})
-})
+router.delete('/:id', delete_quiz)
 
 // Update a quiz
-router.patch('/:id', (request, response) => {
-    response.json({mssg: 'UPDATE a quiz'})
-})
+router.patch('/:id', patch_quiz)
 
 module.exports = router
