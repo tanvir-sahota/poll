60b38ebc46e8baee64ca4c71e8570fd0fd07d2e2
Omar Yahya <omar.yahya@kcl.ac.uk>
2024-02-11 17:39:42 +0000

    Add hashing to passwords before saving to User database

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require ('bcrypt')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
@@ -12,5 +13,17 @@ const userSchema = new Schema({
   },
 
 }, { timestamps: true })
+
+// Hash password before saving to database, via bcrypt
+userSchema.pre('save', function (next) {
+  bcrypt.hash(this.password, 10, (err, hashedPassword) => {
+    if (err) {
+      console.error(err.message)
+      next(err)
+    }
+  this.password = hashedPassword
+  next()
+  })
+})
 
 module.exports = mongoose.model('User', userSchema)
