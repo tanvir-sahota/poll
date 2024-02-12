fbd58ab28ea8b72ea16c298abb1eb6a3e78b4a52
Saihan786 <marshall.s15@hotmail.com>
2024-02-12 15:18:05 +0000

    Add cors for requests from diff hosts like testing

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,28 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const quizRoutes = require('./routes/quizzes')
 const app = express()
-const port = 4000
+const port = 3000
 
 const mongoose = require('mongoose')
+
+const allowEveryOrigin = true
+
+if (!allowEveryOrigin)
+{
+  //Only allows requests from one host for security, enable in production
+  const corsOptions = { 
+    origin : ['http://localhost:3000'], 
+  } 
+  app.use(cors(corsOptions))
+}
+else
+{
+  //Allows CORS from everywhere for development
+  app.use(cors())
+}
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
