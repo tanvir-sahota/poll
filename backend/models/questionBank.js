069112ae2d3ddbb925c06f2d8e7fcb998a23431a
SebKCL <k22008004@kcl.ac.uk>
2024-02-15 15:59:51 +0000

    Create QuestionBank model

---
+++ backend/models/questionBank.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const questionBankSchema = new Schema({
+    questionArray: {
+        type: [{type: mongoose.Schema.Types.ObjectId, ref:"Question"}],
+        required: true
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model("QuestionBank", questionBankSchema)
+
