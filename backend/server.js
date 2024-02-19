a1be1e79a368c8bb76369fddc84c9cbf06d4a7b1
Saihan786 <marshall.s15@hotmail.com>
2024-02-19 19:42:44 +0000

    Separate app and server files

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,15 @@
 require('dotenv').config()
-
-const express = require('express')
-const cors = require('cors')
-const quizRoutes = require('./routes/quizzes')
-const app = express()
-const port = 4000
-
 const mongoose = require('mongoose')
 
-const allowEveryOrigin = true
-
-if (!allowEveryOrigin) {
-    //Only allows requests from one host for security, enable in production
-    const corsOptions = {
-        origin: ['http://localhost:4000'],
-    }
-    app.use(cors(corsOptions))
-} else {
-    //Allows CORS from everywhere for development
-    app.use(cors())
-}
-
-
-app.use(express.json())
-
-app.use('/api/quizzes', quizRoutes)
-
+const app = require('./app')
+const port = 4000
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        console.log('Successfuly connected to database.')
-
-
+        console.log('Successfully connected to database in server.js')
         app.listen(port, () => {
             console.log(`Example app listening on port ${port}`)
         })
-
-        // routes
-
-        // uses routes specified in the quizzes.js file
-
     })
     .catch((err) => {
         console.log(err)
