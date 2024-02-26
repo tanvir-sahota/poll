9cae05279887d0a3b0811f91cc13b7063df8dd9f
harunabukar <harun.abukar@kcl.ac.uk>
2024-02-26 11:11:46 +0000

    "fix question tests which were not working"

--- backend/__tests__/questionTest.js
+++ backend/__tests__/questionTest.js
@@ -1,18 +1,31 @@
 const mongoose = require("mongoose")
 const request = require("supertest")
-
 const app = require("../app")
 const server = require("../server")
 
 require("dotenv").config()
 
 let questionID
+let classID
 
 beforeAll(async () => {
   await mongoose.connect(process.env.MONGO_URI)
 
-  const response = await request(app)
-    .post("/api/questions")
+  //create sample class
+  const classResponse = await request(app)
+    .post("/api/classrooms")
+    .send({
+      title : "title",
+      owner : "owner"
+    })
+    .set({
+      "Content-Type": "application/json"
+    })
+
+    classID = classResponse.body._id
+  //create sample question
+  const questionResponse = await request(app)
+    .post(`/api/questions/${classID}`)
     .send({
       questionAsked: "question",
       options: "5,6,7",
@@ -22,19 +35,18 @@ beforeAll(async () => {
     .set({
       "Content-Type": "application/json"
     })
-    .expect(200)
-  questionID = response.body._id
+  questionID = questionResponse.body._id
 })
 
 afterAll(async () => {
     await mongoose.connection.close()
-    await server.close()
+    server.close()
 }) 
 
-describe("POST /api/questions", () => {
+describe("POST /api/questions/:classID", () => {
     it("should add a question to the database", async () => {
       const response = await request(app)
-        .post(`/api/questions`)
+        .post(`/api/questions/${classID}`)
         .send({
           questionAsked: "What is 3 + 12",
           options: "5,45,56,36,18",
@@ -50,28 +62,28 @@ describe("POST /api/questions", () => {
     })
   })
 
-  describe("GET /api/questions", () => {
+  describe("GET /api/questions/:classID", () => {
     it("should get all the questions in the database", async () => {
       const response = await request(app)
-        .get(`/api/questions`)
+        .get(`/api/questions/${classID}`)
       expect(response.statusCode).toBe(200)
     })
   })
   
-  describe("GET /api/questions/:id", () => {
+  describe("GET /api/questions/:classID/:questionID", () => {
     it("should get the sample question from the database", async () => {
       const response = await request(app)
-        .get(`/api/questions/${questionID}`)
+        .get(`/api/questions/${classID}/${questionID}`)
       expect(response.statusCode).toBe(200)
     })
   })
 
-  describe("PATCH /api/questions/:id", () => {
+  describe("PATCH /api/questions/:classID/:questionID", () => {
 
     it("it should update the sample question multiple times", async () => {
 
       const responseOne = await request(app)
-        .patch(`/api/questions/${questionID}`)
+        .patch(`/api/questions/${classID}/${questionID}`)
         .send({
           questionAsked: "new question",
           options: "9,3,a",
@@ -83,7 +95,7 @@ describe("POST /api/questions", () => {
       expect(responseOne.statusCode).toBe(200)
         
       const responseTwo = await request(app)
-        .patch(`/api/questions/${questionID}`)
+        .patch(`/api/questions/${classID}/${questionID}`)
         .send({
           options: "3,a,6,7,f",
           answers: "6,7"
@@ -95,20 +107,20 @@ describe("POST /api/questions", () => {
     }) 
   }) 
 
-  describe("DELETE /api/questions/:id", () => {
+  describe("DELETE /api/questions/:classID/:questionID", () => {
 
     it("should delete the sample question from database", async () => {
 
       const response = await request(app)
-        .delete(`/api/questions/${questionID}`)
+        .delete(`/api/questions/${classID}/${questionID}`)
         expect(response.statusCode).toBe(200)
     }) 
   }) 
 
-  describe("POST /api/questions", () => {
+  describe("POST /api/questions/:classID", () => {
     it("should return a 422 error as the options doesnt contain all answers", async () => {
       const response = await request(app)
-        .post(`/api/questions`)
+        .post(`/api/questions/${classID}`)
         //4 in answers is not one of the options
         .send({
           questionAsked: "What is 3 + 12",
@@ -123,19 +135,19 @@ describe("POST /api/questions", () => {
     })
   })
   
-  describe("GET /api/questions/:id", () => {
+  describe("GET /api/questions/:classID/:questionID", () => {
     it("should return 404 error as question doesn't exist", async () => {
       const response = await request(app)
       //object doesnt exist in database
-        .get(`/api/questions/invalidID`)
+        .get(`/api/questions/${classID}/invalidID`)
       expect(response.statusCode).toBe(404)
     })
   })
 
-  describe("POST /api/questions", () => {
+  describe("POST /api/questions/:classID", () => {
     it("should return a 400 error as all fields not filled", async () => {
       const response = await request(app)
-        .post(`/api/questions`)
+        .post(`/api/questions/${classID}`)
         //answers field unfilled
         .send({
           questionAsked: "What is 3 + 12",
