4dfac11158f7483fc5c966fe80c6249b7f69467a
Saihan786 <marshall.s15@hotmail.com>
2024-02-27 09:29:19 +0000

    Resolved merges for virtualClassroom and account-creation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { QuizzesContextProvider } from "./context/QuizContext";
 import { QuestionContextProvider } from './context/QuestionContext';
 import { ClassroomContextProvider } from './context/ClassroomContext';
@@ -19,3 +20,8 @@ root.render(
   </React.StrictMode>
 
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
