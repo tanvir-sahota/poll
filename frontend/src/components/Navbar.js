f7bcc6d860cb9a686836b81f832a04fc8991de8f
Omar Yahya <omar.yahya@kcl.ac.uk>
2024-03-04 01:12:18 +0000

    Add Login, Signup links onto navbar, and username when logged in

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
   const { logout } = useLogout()
+  const { user } = useAuthContext()
 
   const handleClick = () => {
     logout()
@@ -16,9 +18,19 @@ const Navbar = () => {
         </Link>
       </div>
       <nav>
-        <div>
-          <button onClick={handleClick}>Log out</button>
-        </div>
+        {user && (
+          <div>
+            <span>{user.username}</span>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+        )}
+        {!user && (
+          <div>
+            <Link to ="/log_in">Login</Link>
+            <Link to ="/sign_up">Signup</Link>
+          </div>
+        )
+        }
       </nav>
     </header>
   )
