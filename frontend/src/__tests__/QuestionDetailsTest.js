5d782e8f1e1ba37d9caabb878663f3b1ff9a53b4
harunabukar <harun.abukar@kcl.ac.uk>
2024-03-18 02:17:51 +0000

    "fix QuestionDetails test"

--- frontend/src/__tests__/QuestionDetailsTest.js
+++ frontend/src/__tests__/QuestionDetailsTest.js
@@ -3,11 +3,20 @@ import userEvent from '@testing-library/user-event'
 import QuestionDetails from "../components/QuestionDetails"
 import { QuestionContextProvider } from "../context/QuestionContext"
 import QuestionForm from '../components/QuestionForm'
+import { useNavigate } from 'react-router-dom'
 
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => (jest.fn()),
+}))
 test('renders QuestionDetails correctly and functionality works correctly', () => {
+
     render(
         <QuestionContextProvider>
-                <QuestionDetails />
+                <QuestionDetails question={{"question": "What is 5+5",
+                                            "options": ["2", "6", "10", "11"],
+                                            "answers": ["Paris"]}}/>
         </QuestionContextProvider>
     )
     
