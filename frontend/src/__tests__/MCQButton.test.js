75ff27e22f3b7783e33dc9746deb717ee4cdf89a
harunabukar <harun.abukar@kcl.ac.uk>
2024-03-18 01:40:13 +0000

    "fix tests for question test, add new test for MCQ Button"

--- frontend/src/__tests__/MCQButton.test.js
+++ frontend/src/__tests__/MCQButton.test.js
@@ -1,30 +1,44 @@
-import {render, userEvent} from '@testing-library/react'
-import MCQButton from '../components/MCQButton'
-import {WebSocket} from 'jest-websocket-mock'
-jest.mock('websocket', () => require('jest-websocket-mock'))
-import userEvent from "@testing-library/user-event";
-
-    const user = userEvent.setup()
-    const mockLecturer = "SM"
-    const mockOption = "option"
-    const websocket = new WebSocket('websocket://localhost:3000')
-beforeAll(() => {
+import React from "react"
+import { fireEvent, render, waitFor } from "@testing-library/react"
+import MCQButton from "../components/MCQButton"
+import io from "socket.io-client"
+import {WebSocket} from 'mock-socket'
 
 
-})
+//create a mock lecturer and mock option
+const mockLecturer = "SM"
+const mockOption = "option"
 
-afterAll(() => {
-    websocket.close()
 
-})
 
-test('should submit an MCQ option', async() => {
-    render(<MCQButton option={mockOption} socket={websocket} lecturer={mockLecturer}/>)
-   
-    
-    websocket.on('submit-answer-MCQ', (mockLecturer, mockOption) => {
-        expect
+jest.mock('socket.io-client')
+
+
+
+//mock a web socket
+const mockSocket = new WebSocket("ws://localhost:3000")
+mockSocket.emit = function () {jest.fn()}
+
+//check if the submit mcq works
+test('submitMCQAnswer emits correct message', async () => {
+    const { getByRole } = render(<MCQButton option={mockOption} socket={mockSocket} lecturer={mockLecturer} />)
+
+    const mockEmit = jest.fn()
+    io.mockImplementation(() => ({
+        emit: mockEmit,
+        // on: jest.fn(),
+        // off: jest.fn(),
+        // close: jest.fn(),
+    }))
+
+    //get the button
+    const submit = getByRole("button")
+    //get the button click
+    fireEvent.click(submit)
+
+    //expect the web socket to work
+    await waitFor(() =>{
+        expect(mockEmit).toHaveBeenCalledWith("submit-answer-MCQ")
     })
-
-    await user.click(screen.queryByRole('button'))
+    
 })
