3c8c9b68ace7effb727fa7f6e8f8eeaf3e522537
Omar Yahya <k22023464@kcl.ac.uk>
2024-03-24 19:30:48 +0000

    Update ClassroomForm test for submission

--- frontend/src/__tests__/ClassroomForm.test.js
+++ frontend/src/__tests__/ClassroomForm.test.js
@@ -1,23 +1,38 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
-//import userEvent from '@testing-library/user-event'
+import { render, screen, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import ClassroomForm from '../../src/components/forms/ClassroomForm'
 import { ClassroomContextProvider } from '../context/ClassroomContext'
 
-test('renders ClassroomForm correctly', () => {
-    render(<ClassroomContextProvider><ClassroomForm /></ClassroomContextProvider>)
+describe('ClassroomForm component', () => {
+  test('renders ClassroomForm correctly', () => {
+      render(<ClassroomContextProvider><ClassroomForm /></ClassroomContextProvider>)
+      fireEvent.click(screen.getByRole('button'))   // need to click yellow button to show the modal
+
+      expect(screen.getByText('Create new classroom')).toBeInTheDocument()
+      expect(screen.getByPlaceholderText('Enter name')).toBeInTheDocument()
+      expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument()
+      expect(screen.getByRole('button', { name: 'Create classroom' })).toBeInTheDocument()
     
-    // Testing elements in form
-    expect(screen.getByRole('button')).toBeInTheDocument()
-    //userEvent.click(screen.getByRole('button'))
-   
-    /*expect(screen.getAllByText('Create new classroom')[0]).toBeInTheDocument()
-    expect(screen.getByPlaceholderText('Enter name')).toBeInTheDocument()
-    expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument()
-    expect(screen.getByRole('button', { name: 'Create classroom' })).toBeInTheDocument()
-  
-    // Testing submitting form
-    const usernameInput = screen.getByPlaceholderText('Enter name')
-    userEvent.type(usernameInput, 'testclassroom')
-    userEvent.click(screen.getByRole('button', { name: 'Create classroom' }))*/
-  })
+    })
+
+  describe("Ensure form submits with correct data", () => {
+    test("Ensure POST request sent after submit (with correct info) is clicked", async () => {
+      const mockCallback = jest.fn()
+      render(<ClassroomContextProvider><ClassroomForm onSubmit={mockCallback()}/></ClassroomContextProvider>);
+      fireEvent.click(screen.getByRole('button'))   // need to click yellow button to show the modal
+
+      const nameInput = screen.getByPlaceholderText('Enter name')
+      const createButton = screen.getByRole('button', { name: 'Create classroom' });
+
+      // Simulate user input
+      userEvent.type(nameInput, 'Test Classroom');
+
+      // Simulate form submission
+      fireEvent.click(createButton)
+
+      // Wait for the POST request to be made
+      expect(mockCallback).toHaveBeenCalled()
+    });
+  });
+})
