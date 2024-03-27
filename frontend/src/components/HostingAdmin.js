4039dc3e48af40caec972835e4690b33019c9b33
SebKCL <k22008004@kcl.ac.uk>
2024-03-27 23:09:35 +0000

    Fix submission tracking

--- frontend/src/components/HostingAdmin.js
+++ frontend/src/components/HostingAdmin.js
@@ -84,9 +84,13 @@ const HostingAdmin = (inputData) => {
                 console.log(`${allAnswers[position]} ${questions[position].question}`)
                 return allAnswers
             })
-            socket.emit("get-number-of-submissions", lecturer, (response) => {
-                setSubmission(response.count)
-            })
+            if(currentQuestion.questionType == "Wh-Question"){
+                setSubmission((prevSubmissions) => {
+                    const newSubmissions = prevSubmissions
+                    return newSubmissions + 1
+                })
+            }
+            
         }
         socket.addEventListener("recieve-answer-text", receiveTextHandler)
         console.log("Added text event handler")
@@ -197,7 +201,6 @@ const HostingAdmin = (inputData) => {
 
             shouldRenderPrevious(tempPosition + 1)
             shouldRenderNext(tempPosition + 1)
-
         }
     }
 
@@ -208,7 +211,9 @@ const HostingAdmin = (inputData) => {
         } else {
             nextButton.hidden = false
         }
-
+        if(currentQuestion.questionType == "Wh-Question"){
+            setSubmission(answers[position][0] + answers[position][1])
+        }
     }
 
     const handlePrev = async () => {
@@ -220,7 +225,6 @@ const HostingAdmin = (inputData) => {
             shouldRenderPrevious(tempPosition -1)
             shouldRenderNext(tempPosition -1)
         }
-        setSubmission(0)
     }
 
     const shouldRenderPrevious = async (newPos = 0) => {
@@ -230,6 +234,9 @@ const HostingAdmin = (inputData) => {
             prevButton.hidden = true
         } else {
             prevButton.hidden = false
+        }
+        if(currentQuestion.questionType == "Wh-Question"){
+            setSubmission(answers[position][0] + answers[position][1])
         }
     }
 
