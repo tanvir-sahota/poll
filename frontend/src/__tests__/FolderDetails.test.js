e437695453c586c82c9285db78af1a07d4b8d60a
Siqi Li <1114673087@qq.com>
2024-03-28 00:22:41 +0000

    fix FolderDetails.test and QuestionDetails.test

--- frontend/src/__tests__/FolderDetails.test.js
+++ frontend/src/__tests__/FolderDetails.test.js
@@ -30,7 +30,7 @@ describe('FolderDetails component', () => {
 
     expect(screen.getByText(mockFolder.title)).toBeInTheDocument();
     expect(screen.getByText('Mock Folder')).toBeInTheDocument();
-    expect(screen.getByText('delete')).toBeInTheDocument();
+    expect(screen.getByText('Delete')).toBeInTheDocument();
 
   });
 
@@ -41,7 +41,7 @@ describe('FolderDetails component', () => {
         <FolderDetails folder={mockFolder} classID="Classroom 101" onSubmit={mockCallback()} />
         </Router>
     );
-    const deleteButton = screen.getByText('delete');
+    const deleteButton = screen.getByText('Delete');
     fireEvent.click(deleteButton)
     expect(mockCallback).toHaveBeenCalled()
   });
