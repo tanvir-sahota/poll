05d939c283d224a087d53f90488822a4e77cb834
Saihan786 <marshall.s15@hotmail.com>
2024-03-28 11:42:52 +0000

    Fix Navbar tests

--- frontend/src/__tests__/Navbar.test.js
+++ frontend/src/__tests__/Navbar.test.js
@@ -1,62 +1,93 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import { AuthContextProvider } from '../context/AuthContext';
+
+const CheckLogoutUsed = () => {
+  return (
+    <h3>success logout</h3>
+  )
+}
+const CheckNavigateUsed = () => {
+}
+
+// mock models
+
+const mockUser = {
+  username: "username",
+  password: "password",
+}
+
+
+// mock context
+
+jest.mock('../hooks/useAuthContext', () => ({
+  useAuthContext: () => ({user: mockUser})
+}))
+
+jest.mock('../hooks/useAuthContext', () => ({
+  useAuthContext: () => ({user: mockUser})
+}))
+
+
+// mock functions
 
 jest.mock('../hooks/useLogout', () => ({
-  useLogout: () => ({
-    logout: jest.fn(),
-  }),
+  useLogout: () => ({logout: CheckLogoutUsed}),
+}));
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(CheckNavigateUsed()),
+  // useNavigate: () => jest.fn(),
 }));
 
 
-describe('Navbar Component', () => {
-  test('renders Navbar component with logged-in user', () => {
-    render(
-      <AuthContextProvider value={{ user: { username: 'testUser' } }}>
-        <BrowserRouter>
-          <Navbar />
-        </BrowserRouter>
-      </AuthContextProvider>
-    );
+// render components
 
-    const usernameElement = screen.getByText('testUser');
-    expect(usernameElement).toBeInTheDocument();
+const MockNavbar = () => {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+    </BrowserRouter>
+  )
+}
 
-    const logoutButton = screen.getByText('Log out');
-    expect(logoutButton).toBeInTheDocument();
+
+
+
+// tests
+
+describe('Appearance tests (logged-in user)', () => {
+  test('for pollname', () => {
+    render(MockNavbar());
+
+    const pollName = screen.getByText('PollAnywhere');
+    expect(pollName).toBeInTheDocument();
+
   });
+  test('for menu', () => {
+    render(MockNavbar());
 
-  test('renders Navbar component with logged-out user', () => {
-    render(
-      <AuthContextProvider value={{ user: null }}>
-        <BrowserRouter>
-          <Navbar />
-        </BrowserRouter>
-      </AuthContextProvider>
-    );
-
-    const loginLink = screen.getByText('Login');
-    expect(loginLink).toBeInTheDocument();
-
-    const signupLink = screen.getByText('Signup');
-    expect(signupLink).toBeInTheDocument();
+    const menu = screen.queryByRole('button');
+    expect(menu).toBeInTheDocument();
   });
+  test('for menu items', async () => {
+    render(MockNavbar());
+    const menu = screen.queryByRole('button');
+    
+    await act(async () => {    
+      fireEvent.click(menu)
+    })
 
-  test('clicking logout button calls logout function', () => {
+    const dashboardText = screen.getByText('Dashboard')
+    const logOutText = screen.getByText('Log out')
+    const qrCodeText = screen.getByText('QR code')
 
-    render(
-      <AuthContextProvider value={{ user: { username: 'testUser' } }}>
-        <BrowserRouter>
-          <Navbar />
-        </BrowserRouter>
-      </AuthContextProvider>
-    );
-
-    const logoutButton = screen.getByText('Log out');
-    fireEvent.click(logoutButton);
-
-    expect(logoutMock).toHaveBeenCalled();
+    expect(dashboardText).toBeInTheDocument()
+    expect(logOutText).toBeInTheDocument()
+    expect(qrCodeText).toBeInTheDocument()
   });
 });
